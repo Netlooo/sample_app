1337b9f11e299ab0dbd384108c5c4fc109f3170f
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-05-05 08:29:24 +0000

    [finish]3-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c8962d3776cf4c01a8d5cf628453afb0.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
