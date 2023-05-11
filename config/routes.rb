a0260aac826c9d664d3c6ad8646651570d99074e
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-05-11 13:47:21 +0000

    [Finish] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists'=>'lists#index'
-  get '/top' => 'homes#top'
-  get 'lists/:id'=>'lists#show', as: 'list'
-  get 'lists/:id/edit'=>'lists#edit', as: 'edit_list'
-  patch 'lists/:id'=>'lists#update', as: 'update_list'
-　delete 'lists/:id'=>'lists#destory', as: 'destroy_list'
+
+  get 'top'=>'homes#top'
+  resources :lists
 end
